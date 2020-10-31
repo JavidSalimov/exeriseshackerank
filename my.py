e5e7229db6904dcec6aba37b3658c93c0cfb7734
JavidSelimov12345 <44189780+JavidSelimov12345@users.noreply.github.com>
2020-10-31 17:21:50 +0400

    my python exercise from hackerank

---
+++ my.py
@@ -0,0 +1,18 @@
+def countOfPairs(n,arr):
+    dict_={}
+    x=set()
+    cou=0
+    for i in range(0,n):
+        w=arr[i]
+        dict_[w]=arr.count(w)
+        x.add(w)
+    for i in x:
+        a=dict_[i] 
+        if a<=2 and a%2==0:
+            cou=cou+a//2 
+        elif a<=2 and a%2!=0:
+            cou=cou+(a-1)//2
+
+    print(cou)    
+
+countOfPairs(7,[2,2,3,3,4,4,1])              
