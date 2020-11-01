4f7cdbc56d655ff1584f9c786cd0838526e00ed3
JavidSelimov12345 <44189780+JavidSelimov12345@users.noreply.github.com>
2020-11-02 00:02:23 +0400

    added list exercise solution

---
+++ lists.py
@@ -0,0 +1,20 @@
+if __name__ == '__main__':
+    N = int(input())
+    a=[]
+
+    for n in range(N):
+        z=list(input().split())
+        if z[0]=='insert':
+            a.insert(int(z[1]),int(z[2]))
+        elif z[0]=='print':
+            print(a) 
+        elif z[0]=='remove':
+            a.remove(int(z[1])) 
+        elif z[0]=='append':
+            a.append(int(z[1])) 
+        elif z[0]=='sort':
+            a.sort()  
+        elif z[0]=='pop':
+            a.pop()
+        elif z[0]=='reverse':
+            a.reverse()    
